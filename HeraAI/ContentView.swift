04888acc2105868f73627f597832a078b6d69f2f
arvas <arvas>
2024-07-08 22:32:56 +0300

    Initial Commit

---
+++ HeraAI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HeraAI
+//
+//  Created by ali arvas on 8.07.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HeraAI/HeraAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeraAIApp.swift
+//  HeraAI
+//
+//  Created by ali arvas on 8.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct HeraAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
